2c276a097ebc9946e4cc5b7dca3205c35864e865
Mayur Dogne <82083444+1809mayur@users.noreply.github.com>
2021-05-07 14:37:30 +0530

    Update settings.py

--- lcotubers/settings.py
+++ lcotubers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vu0&=1g197%6mx745!j#e12z&#=@9i88utzl4&no%n5*&9o2%)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','https://hireyoutuber.herokuapp.com/']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
